5fa1b086b09febccfd9c665dfbc543f3ab7ebf21
Kleber Alves <klebernascimento@outlook.com.br>
2020-06-03 16:32:01 -0300

    Correção dos bugs pelo instrutor, e finalizando o css do formulario

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -24,13 +24,15 @@ function getCities(event){
   stateInput.value = event.target.options[indexOfSelectedState].text
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  citySelect.innerHTML = '<option value>Selecione a Cidade</option>'
+  citySelect.disabled = true
 
   fetch(url)
   .then( res =>  res.json() )
   .then( cities => {
-    citySelect.innerHTML = ''
+    
     for(city of cities){
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
     }
 
     citySelect.disabled = false
